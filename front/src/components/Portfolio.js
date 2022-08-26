8e62e21f4d4affc15e2dd9bb7cfa59dcb0fdba8f
임지원 <9universe9th@gmail.com>
2022-08-26 15:24:37 +0900

    CRUD 구현

--- front/src/components/Portfolio.js
+++ front/src/components/Portfolio.js
@@ -3,8 +3,9 @@ import { useNavigate, useParams } from "react-router-dom";
 import { Container, Col, Row } from "react-bootstrap";
 
 import { UserStateContext } from "../App";
-import * as Api from "../api";
+import * as Api from "../apiMock";
 import User from "./user/User";
+import Certificates from "./Certificate/Certificates";
 
 function Portfolio() {
   const navigate = useNavigate();
@@ -54,18 +55,17 @@ function Portfolio() {
   return (
     <Container fluid>
       <Row>
-        <Col md="3" lg="3">
+        <Col ms={3} md="3" lg="3">
           <User
             portfolioOwnerId={portfolioOwner.id}
             isEditable={portfolioOwner.id === userState.user?.id}
           />
         </Col>
-        <Col>
-
-          <div style={{ textAlign: "center" }}>
-            학력 목록, 수상이력 목록, 프로젝트 목록, 자격증 목록 만들기
-          </div>
-
+        <Col ms md="auto" lg="8">
+          <Certificates
+            portfolioOwnerId={portfolioOwner.id}
+            isEditable={portfolioOwner.id === userState.user?.id}
+          />
         </Col>
       </Row>
     </Container>
